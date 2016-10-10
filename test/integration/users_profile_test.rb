b1dc2d49282fb2c236b7a9c67fc2b8b46e2464ba
Sander Sijbrandij <ssijbrandij@gmail.com>
2016-10-10 13:52:41 +0200

    Add user following with redirects

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -5,6 +5,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
+    log_in_as(@user)
   end
 
   test "profile display" do
@@ -18,5 +20,27 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    # Own Profile, before following
+    assert_select 'strong#following', text: '2'
+    assert_select 'strong#followers', text: '2'
+    assert_select 'div#follow_form', count: 0
+
+    # Other Profile, after following
+    @user.follow(@other_user)
+    get user_path(@other_user)
+
+    assert_select 'strong#followers', text: '1'
+    assert_select 'strong#following', text: '1'
+    assert_select 'div#follow_form', count: 1
+    assert_match  'Unfollow', response.body
+
+    # Unfollow and reverse
+    @user.unfollow(@other_user)
+    @other_user.unfollow(@user)
+    get user_path(@other_user)
+
+    assert_select 'strong#followers', text: '0'
+    assert_select 'strong#following', text: '0'
+    assert_match  'Follow', response.body
   end
 end
